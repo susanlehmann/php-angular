47ff9937b03865e3e926fb53f64a1eeebd6fe8f9
anhnm <anhnm102@gmail.com>
2019-01-14 13:33:13 +0700

    Revert "commit frontend" because of unfinished code
    This reverts commit 936b981b9a51957409c52b8435b044b9f46709a2.
    
    # Conflicts:
    #	frontend/package.json

--- frontend/src/app/layout/charts/charts.module.ts
+++ frontend/src/app/layout/charts/charts.module.ts
@@ -5,16 +5,9 @@ import { ChartsModule as Ng2Charts } from 'ng2-charts';
 import { ChartsRoutingModule } from './charts-routing.module';
 import { ChartsComponent } from './charts.component';
 import { PageHeaderModule } from '../../shared';
-import { NgxTuiCalendarModule } from 'ngx-tui-calendar';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
-    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule, NgxTuiCalendarModule.forRoot(),
-        CalendarModule.forRoot({
-            provide: DateAdapter,
-            useFactory: adapterFactory
-          })],
+    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule],
     declarations: [ChartsComponent]
 })
 export class ChartsModule {}
